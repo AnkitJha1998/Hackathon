f66b1f370b21125b4c93a2c6161f52ef076c5d5f
NikihlPopli <npopli98@gmail.com>
2018-03-24 00:20:57 +0530

    basic sharing using client and server(txt file only)

---
+++ client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import socket
+
+TCP_IP = 'localhost'
+TCP_PORT = 9001
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+with open('received_file', 'wb') as f:
+	print("file opened")
+	while True:
+		print("Receiving data...")
+		data = s.recv(BUFFER_SIZE)
+		print('data=%s', (data))
+		if not data:
+			f.close()
+			print('file closed')
+			break
+
+		f.write(data)
+
+print('Successfully get the file')
+s.close()
+print('connection refused')
